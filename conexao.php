7ae6156d33f02510c79dd9a1722771c279031fd1
Marco <m.miranda2015@outlook.com>
2022-10-14 13:08:30 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 ﻿<?php
 
     // Dados de acesso ao banco de dados.
-    define('HOST', 'sigmaplast.mysql.dbaas.com.br');
-    define('USUARIO', 'sigmaplast');
-    define('SENHA', 'Yabb11');
-    define('DB', 'sigmaplast');
+    define('HOST', '');
+    define('USUARIO', '');
+    define('SENHA', '');
+    define('DB', '');
     
     // Criando conexão com o banco de dados.
     $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
